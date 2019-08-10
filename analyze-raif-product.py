ad727467d5dc581ea2db65d6520e1afe19312fb6
Oleg Tsarkov <olegts@mbp-oleg.home>
2019-08-10 12:40:42 +0200

    Add currency

--- analyze-raif-product.py
+++ analyze-raif-product.py
@@ -4,15 +4,19 @@ import sys
 filename = sys.argv[1]
 all_descs = defaultdict(int)
 money_sum = defaultdict(int)
+currencies = defaultdict(str)
 with open(filename, 'r') as f:
     first_line = True
     for line in f.read().decode('windows-1251').encode('utf8').strip().split('\n'):
         if not(first_line):
             description = line.split(';')[1]
+            currency = line.split(';')[2]
             money = line.split(';')[3]
             if sum(letter >= 'а' and letter <= 'я' for letter in description) > 0 :
                 all_descs[description] += 1
                 money_sum[description] += float(money.replace(' ', ''))
+                currencies[description] = currency;
         first_line = False
 print("decription\tcount\ttotal_money")
-print("\n".join('{desc}\t{count}\t{money}'.format(desc=v[1], count=v[0], money=money_sum[v[1]]) for v in sorted([(all_descs[k], k) for k in all_descs])))
+print("\n".join('{desc}\t{count}\t{money} {currency}'.format(desc=v[1], count=v[0],
+    money=money_sum[v[1]], currency=currencies[v[1]]) for v in sorted([(all_descs[k], k) for k in all_descs])))
